5c73176b3a54691a044657d1c52e6e8a8edf2e01
Abner Eger <abner.juda.eger.santana@gmail.com>
2024-01-25 10:29:17 -0300

    docs: :memo: add documentation

--- main.py
+++ main.py
@@ -17,7 +17,12 @@ fonte_small = pygame.font.SysFont(None, 20)
 fonte = pygame.font.SysFont(None, 30)
 fonte_titulo = pygame.font.SysFont(None, 50)
 
-def mostrar_pontuacao(pontuacao):
+def mostrar_pontuacao(pontuacao: int):
+    """Mostra a pontuação do jogador durante o jogo.
+
+    Args:
+        pontuacao (int): Quantidade de maçãs que comeu.
+    """
     # Ajustar a largura do quadrado arredondado com base no número de dígitos na pontuação
     largura_quadrado = 150 + 10 * (len(str(pontuacao)) - 1)
     
@@ -29,10 +34,16 @@ def mostrar_pontuacao(pontuacao):
     texto = fonte.render("Pontuação: " + str(pontuacao), True, (0, 0, 0))
     tela.blit(texto, [20, 20])
     
-def game_over(result, pontuacao):
-    cor_clara = (251, 251, 251)  # Verde claro
-    cor_escura = (205, 205, 205)   # Verde escuro
-    borda = (150, 150, 150)
+def game_over(result: str, pontuacao: int):
+    """Tela de Game Over quando o jogador perde ou ganha.
+
+    Args:
+        result (str): Se ganhou ou perdeu.
+        pontuacao (int): Quantidade de maçãs que comeu.
+    """
+    cor_clara = (251, 251, 251)
+    cor_escura = (205, 205, 205)
+    borda = (150, 150, 150) # Cor da borda
     tela.fill((0, 0, 0))
     
     while True:
@@ -40,21 +51,19 @@ def game_over(result, pontuacao):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    return  # Retorna para a função que chamou game_over
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if largura_tela / 2 - 150 / 2 - 90 <= event.pos[0] <= largura_tela / 2 - 150 / 2 - 90 + 150 \
                         and altura_tela / 2 - 35 / 2 + 50 <= event.pos[1] <= altura_tela / 2 - 35 / 2 + 50 + 35:
                     # Clicou no botão "Recomeçar"
-                    jogo()
+                    resultado, pontuacao = jogo()
+                    game_over(resultado, pontuacao)
 
                 elif largura_tela / 2 - 150 / 2 + 90 <= event.pos[0] <= largura_tela / 2 - 150 / 2 + 90 + 150 \
                         and altura_tela / 2 - 35 / 2 + 50 <= event.pos[1] <= altura_tela / 2 - 35 / 2 + 50 + 35:
                     # Clicou no botão "Menu"
                     main()
         
-        # Preencher a tela com um padrão xadrez de verde claro e escuro
+        # Preencher a tela com um padrão xadrez de cinza claro e escuro
         pygame.draw.rect(tela, borda, [70, 70, 660, 460])
         for i in range(80, largura, 20):
             for j in range(80, altura, 20):
@@ -72,10 +81,13 @@ def game_over(result, pontuacao):
         tela.blit(titulo, titulo_rect)
         
         # Desenha se ganhou ou perdeu
-        
         result_text = fonte_small.render("Parabéns! Você venceu o jogo." if result == "venceu" else "Que pena, você perdeu! Mais sorte da próxima vez.", True, (0, 0, 0))
-        result_rect = result_text.get_rect(center=(largura_tela / 2, altura_tela / 2))
+        result_rect = result_text.get_rect(center=(largura_tela / 2, altura_tela / 2 - 10))
+        tela.blit(result_text, result_rect)
         
+        # Desenha sua pontuação
+        result_text = fonte.render(f"Pontuação: {pontuacao}", True, (0, 0, 0))
+        result_rect = result_text.get_rect(center=(largura_tela / 2, altura_tela / 2 + 15))
         tela.blit(result_text, result_rect)
         
         # Desenha o botão de recomeçar
@@ -100,8 +112,57 @@ def game_over(result, pontuacao):
         
         pygame.display.update()
         relogio.tick(ticks)
+    
+def jogo() -> tuple[str, int]:
+    """Tela do jogo.
+
+    Returns:
+        str: Resultado: "venceu" se ganhou o jogo ou "perdeu" se perdeu o jogo.
+        int: Pontuação: Quantidade de maçãs que você comeu.
+    """
+    snake = Snake(largura, altura)
+    food = Food(largura, altura, snake)
+    
+    cor_clara = (251, 251, 251)
+    cor_escura = (205, 205, 205)
+    borda = (150, 150, 150)
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            snake.mudar_direcao(event)
+        
+        # Se a variavel game_over for True ou o corpo da cobra for maior ou igual que o tamanho do mapa ele finaliza o jogo
+        if snake.game_over or largura * altura <= snake.comprimento:
+            return ("venceu", snake.comprimento) if largura * altura <= snake.comprimento else ("perdeu", snake.comprimento)
+        
+        snake.mover()
+        snake.verificar_colisao_comida(food)
+        snake.verificar_colisao()
+        
+        # Preencher a tela com um padrão xadrez de cinza claro e escuro
+        pygame.draw.rect(tela, borda, [70, 70, 660, 460])
+        for i in range(80, largura, 20):
+            for j in range(80, altura, 20):
+                cor = cor_clara if (i + j) % 40 == 0 else cor_escura
+                pygame.draw.rect(tela, cor, [i, j, 20, 20])
+        
+        # Desenha os elementos na tela
+        food.desenhar(tela)
+        snake.desenhar(tela)
+        mostrar_pontuacao(snake.comprimento - 2)
+        
+        pygame.display.update()
+        relogio.tick(ticks)
 
 def main():
+    """Tela principal do jogo."""
+    cor_clara = (251, 251, 251)
+    cor_escura = (205, 205, 205)
+    borda = (150, 150, 150)
+        
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -111,7 +172,8 @@ def main():
                 if largura_tela / 2 - 150 / 2 <= event.pos[0] <= largura_tela / 2 - 150 / 2 + 150 \
                         and altura_tela / 2 - 35 / 2 + 50 <= event.pos[1] <= altura_tela / 2 - 35 / 2 + 50 + 35:
                     # Clicou no botão "Jogar"
-                    jogo()
+                    resultado, pontuacao = jogo()
+                    game_over(resultado, pontuacao)
 
                 elif largura_tela / 2 - 150 / 2 <= event.pos[0] <= largura_tela / 2 - 150 / 2 + 150 \
                         and altura_tela / 2 - 35 / 2 + 100 <= event.pos[1] <= altura_tela / 2 - 35 / 2 + 100 + 35:
@@ -119,10 +181,7 @@ def main():
                     pygame.quit()
                     quit()
         
-        # Preencher a tela com um padrão xadrez de verde claro e escuro
-        cor_clara = (251, 251, 251)  # Verde claro
-        cor_escura = (205, 205, 205)   # Verde escuro
-        borda = (150, 150, 150)
+        # Preencher a tela com um padrão xadrez de cinza claro e escuro
         pygame.draw.rect(tela, borda, [70, 70, 660, 460])
         for i in range(80, largura, 20):
             for j in range(80, altura, 20):
@@ -139,7 +198,7 @@ def main():
         titulo_rect = titulo.get_rect(center=(largura_tela / 2, altura_tela / 2 - 50))
         tela.blit(titulo, titulo_rect)
 
-        # Desenha o botão de iniciar
+        # Desenha o botão de Jogar
         iniciar = fonte.render("Jogar", True, (0, 0, 0))
         texto_rect = iniciar.get_rect(center=(largura_tela / 2, altura_tela / 2 + 50))
         rect = pygame.Rect(largura_tela / 2 - 150 / 2, altura_tela / 2 - 35 / 2 + 50, 150, 35)
@@ -157,42 +216,6 @@ def main():
 
         pygame.display.update()
         relogio.tick(ticks)
-    
-def jogo():
-    snake = Snake(largura, altura)
-    food = Food(largura, altura, snake)
-    
-    cor_clara = (251, 251, 251)  # Verde claro
-    cor_escura = (205, 205, 205)   # Verde escuro
-    borda = (150, 150, 150)
-    
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            snake.mudar_direcao(event)
-        
-        if snake.game_over or largura * altura <= snake.comprimento:
-            break
-        
-        snake.mover()
-        snake.verificar_colisao_comida(food)
-        snake.verificar_colisao()
-        
-        # Preencher a tela com um padrão xadrez de verde claro e escuro
-        pygame.draw.rect(tela, borda, [70, 70, 660, 460])
-        for i in range(80, largura, 20):
-            for j in range(80, altura, 20):
-                cor = cor_clara if (i + j) % 40 == 0 else cor_escura
-                pygame.draw.rect(tela, cor, [i, j, 20, 20])
-        
-        food.desenhar(tela)
-        snake.desenhar(tela)
-        mostrar_pontuacao(snake.comprimento - 2)
-        
-        pygame.display.update()
-        relogio.tick(ticks)
-    game_over("venceu" if largura * altura <= snake.comprimento else "perdeu", snake.comprimento)
-    
-main()
+
+if __name__ == "__main__":
+    main()
